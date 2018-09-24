87f6b71610fb2df285ebbb71e1a2d1a30e6844a6
Paul Clark <clark.paul123@gmail.com>
2018-09-24 20:51:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,14 +3,14 @@
 def fizzbuzz(num) 
   if 
     (num % 3 == 0 && num % 5 == 0) 
-    puts "Fizzbuzz"
+    return "Fizzbuzz"
   elsif
     num % 3 == 0 
-    puts "Fizz"
+    return "Fizz"
   elsif
     num % 5 == 0 
-    puts "Buzz"
+    return "Buzz"
   else 
-    puts nil
+    return nil
   end
 end
